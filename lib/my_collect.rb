ef8b468f45aa418c627f92b3b4f49f623918a117
Charles <charlesangeloplai@gmail.com>
2020-11-13 23:20:01 -0500

    commit tree 12cc1f51cbd11b69ae1dc1b05bef1b3287e32bd4

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+collection = []
 
+def my_collect(collection)
+    if !collection.empty?
+        index = 0
+        result = []
 
+        while index < collection.length
+            result << yield(collection[index])
+            index += 1
+        end
+
+        result
+    end
+end
+
+my_collect(collection) do |element|
+    if element == "Tim" || element == "Tom" || element == "Jim"
+        element = element.first
+    else
+        element = element.upcase
+    end
+end
